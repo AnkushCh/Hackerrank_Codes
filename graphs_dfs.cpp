dd71267265eba171b6a8b27c8c460c7d7d28ddad
AnkushCh <47393801+AnkushCh@users.noreply.github.com>
2021-03-30 00:21:10 +0530

    complete code

--- graphs_dfs.cpp
+++ graphs_dfs.cpp
@@ -5,8 +5,44 @@
 #define pb push_back
 using namespace std;
 
+void edge(v adj[], int u, int vi)
+{
+    adj[u].pb(vi);
+    adj[vi].pb(u);
+}
+void dfsutil(int u, v adj[], vector<bool>&b)
+{
+    b[u] = true;
+    cout << u <<" ";
+    for(int i = 0; i < adj[u].size(); i++)
+    {
+        if(adj[u][i] == false)
+        dfsutil(adj[u][i],adj,b);
+    }
+}
 
+void dfs(v adj[], int n)
+{
+    vector<bool>b(n,false);
+    f(u,n)
+    {
+        if(b[u] == false)
+        dfsutil(u,adj,b);
+    }
+}
 int main()
 {
+    int n = 5;
+    v adj[n];
+    edge(adj, 0, 1);
+    edge(adj, 0, 4);
+    edge(adj, 1, 2);
+    edge(adj, 1, 3);
+    edge(adj, 1, 4);
+    edge(adj, 2, 3);
+    edge(adj, 3, 4);
+    dfs(adj,n);
+    
+    return 0;
 
 }
